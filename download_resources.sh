926464e4154e233dc0a8445f34e7a5cc3eeff173
jason <jason@johnson-net.ch>
2021-05-31 11:36:09 +0000

    added download_resources.sh file

---
+++ download_resources.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download the files to make go gen work
+
+curl -sLOJ https://raw.githubusercontent.com/aztfmod/terraform-provider-azurecaf/master/gen.go
+sed -i 's/azurecaf/swissre/g' gen.go
+curl -sLOJ https://raw.githubusercontent.com/aztfmod/terraform-provider-azurecaf/master/resourceDefinition.json
+curl -sLOJ https://raw.githubusercontent.com/aztfmod/terraform-provider-azurecaf/master/resourceDefinition_out_of_docs.json

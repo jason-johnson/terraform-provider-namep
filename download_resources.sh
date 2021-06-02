1ea264846eb6a839a8dad8aaa78ddb73096a7997
jason <jason@johnson-net.ch>
2021-06-02 18:19:23 +0000

    use our module name instead of template one

--- download_resources.sh
+++ download_resources.sh
@@ -3,6 +3,6 @@
 # Download the files to make go gen work
 
 curl -sLOJ https://raw.githubusercontent.com/aztfmod/terraform-provider-azurecaf/master/gen.go
-sed -i 's/azurecaf/swissre/g' gen.go
+sed -i 's/azurecaf/internal\/provider/g' gen.go
 curl -sLOJ https://raw.githubusercontent.com/aztfmod/terraform-provider-azurecaf/master/resourceDefinition.json
 curl -sLOJ https://raw.githubusercontent.com/aztfmod/terraform-provider-azurecaf/master/resourceDefinition_out_of_docs.json
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/hashicorp/terraform-provider-scaffolding
+module registry.terraform.io/jason-johnson/namep
 
 go 1.15
 

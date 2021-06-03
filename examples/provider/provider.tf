e9d1da34d0cee29266360646653a745bcae88147
jason <jason@johnson-net.ch>
2021-06-03 15:40:11 +0000

    make optional dash example more obvious

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -2,9 +2,11 @@ provider "namep" {
   slice_string     = "MYAPP DEV"
   default_location = "westeurope"
   extra_tokens = {
-    branch = "uxx1"
+    branch = var.branch_name
   }
   resource_formats = {
     azurerm_resource_group = "#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}#{-BRANCH}-#{NAME}"
   }
 }
+
+# NOTE: if branch name is an empty string neither it nor the dash will show up in the name

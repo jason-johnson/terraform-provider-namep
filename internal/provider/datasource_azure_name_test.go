d0ab81f4d8b69bbae69ea8676a0b984db7d9d13c
Jason <jason@johnson-net.ch>
2022-11-19 10:21:53 +0000

    add new test case for extra token override

--- internal/provider/datasource_azure_name_test.go
+++ internal/provider/datasource_azure_name_test.go
@@ -68,12 +68,29 @@ func TestAccDataSourceAzureName_custom_type_fmt(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceAzureName_override_extra_token(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAzureName_override_extra_token,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.namep_azure_name.rg", "result", "rg-myapp-dev-weu-mygroup"),
+					resource.TestCheckResourceAttr("data.namep_azure_name.saa", "result", "unsetmyappdevweusa1"),
+					resource.TestCheckResourceAttr("data.namep_azure_name.sab", "result", "staccmyappdevweusa2"),
+				),
+			},
+		},
+	})
+}
+
 // test configurations
 
 const testAccDataSourceAzureName_default_rg = `
 data "namep_azure_name" "foo" {
   name = "mygroup"
-	location = "westeurope"
+  location = "westeurope"
   type = "azurerm_resource_group"
 }
 `
@@ -81,7 +98,7 @@ data "namep_azure_name" "foo" {
 const testAccDataSourceAzureName_default_sa = `
 data "namep_azure_name" "foo" {
   name = "myacct"
-	location = "westeurope"
+  location = "westeurope"
   type = "azurerm_storage_account"
 }
 `
@@ -100,13 +117,13 @@ provider "namep" {
 
 data "namep_azure_name" "rg" {
   name = "mygroup"
-	location = "westeurope"
+  location = "westeurope"
   type = "azurerm_resource_group"
 }
 
 data "namep_azure_name" "wapp" {
   name = "myapp"
-	location = "westeurope"
+  location = "westeurope"
   type = "azurerm_app_service"
 }
 `
@@ -118,8 +135,8 @@ provider "namep" {
   extra_tokens = {
     branch = "uxx1"
   }
-	default_resource_name_format = "#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}#{-BRANCH}-#{NAME}"
-	default_nodash_name_format = "#{TOKEN_1}#{TOKEN_2}#{SHORT_LOC}#{BRANCH}#{NAME}"
+  default_resource_name_format = "#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}#{-BRANCH}-#{NAME}"
+  default_nodash_name_format = "#{TOKEN_1}#{TOKEN_2}#{SHORT_LOC}#{BRANCH}#{NAME}"
   resource_formats = {
     my_type = "thing-#{TOKEN_2}-#{SHORT_LOC}#{-BRANCH}-#{NAME}"
   }
@@ -127,13 +144,44 @@ provider "namep" {
 
 data "namep_azure_name" "rg" {
   name = "mygroup"
-	location = "westeurope"
+  location = "westeurope"
   type = "azurerm_resource_group"
 }
 
 data "namep_azure_name" "custom" {
   name = "mycustom"
-	location = "westeurope"
+  location = "westeurope"
   type = "my_type"
 }
+`
+
+const testAccDataSourceAzureName_override_extra_token = `
+provider "namep" {
+  slice_string     = "MYAPP DEV"
+  default_location = "westeurope"
+  extra_tokens = {
+    myslug = "unset"
+  }
+	default_resource_name_format = "#{SLUG}-#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}-#{NAME}"
+	default_nodash_name_format = "#{MYSLUG}#{TOKEN_1}#{TOKEN_2}#{SHORT_LOC}#{NAME}"
+}
+
+data "namep_azure_name" "rg" {
+  name = "mygroup"
+  location = "westeurope"
+  type = "azurerm_resource_group"
+}
+
+data "namep_azure_name" "saa" {
+  name = "sa1"
+  location = "westeurope"
+  type = "azurerm_storage_account"
+}
+
+data "namep_azure_name" "sab" {
+	name = "sa2"
+	location = "westeurope"
+	type = "azurerm_storage_account"
+	myslug = "stacc"
+  }
 `

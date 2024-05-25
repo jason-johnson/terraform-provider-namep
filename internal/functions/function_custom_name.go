6a2ee97c4e629f02ec5fd1f7239b143919c9b833
jason <jason@johnson-net.ch>
2024-05-25 19:34:56 +0000

    changing up interface and made initial code for it

--- internal/functions/function_custom_name.go
+++ internal/functions/function_custom_name.go
@@ -24,13 +24,21 @@ func (f *CustomNameFunction) Metadata(ctx context.Context, req function.Metadata
 func (f *CustomNameFunction) Definition(ctx context.Context, req function.DefinitionRequest, resp *function.DefinitionResponse) {
 	resp.Definition = function.Definition{
 		Summary:     "Generate an custom name",
-		Description: "This function creates a name for an azure resource.\nThe format will be used based on the the resource type selected and the appropriate format string.",
+		Description: "This function creates a name for a custom resource.\nThe format will be used based on the the resource type selected and the appropriate format string.",
 
 		Parameters: []function.Parameter{
-			function.DynamicParameter{
-				Name:        "params",
-				Description: "Function parameters.  Valid keys are 'name', 'type', 'location', and 'extra_tokens'.",
+			function.StringParameter{
+				Name:        "resource_type",
+				Description: "Type of resource to create a name for (required for selecting format and certain variables)",
 			},
+			function.DynamicParameter{
+				Name:        "config",
+				Description: "Function Configuration.  Valid keys are 'name', 'type', 'location', and 'extra_tokens'.",
+			},
+		},
+		VariadicParameter: function.DynamicParameter{
+			Name:        "overrides",
+			Description: "Function Configuration Overrides.",
 		},
 
 		Return: function.StringReturn{},
@@ -38,28 +46,22 @@ func (f *CustomNameFunction) Definition(ctx context.Context, req function.Defini
 }
 
 func (f *CustomNameFunction) Run(ctx context.Context, req function.RunRequest, resp *function.RunResponse) {
-	var dynamicArg types.Dynamic
+	var configArg types.Dynamic
+	var overridesArgs []types.Dynamic
 	var resourceType string
 	var name string
 	var location string
 	extraTokens := map[string]string{}
-	resourceTypeSeen := false
 
-	resp.Error = function.ConcatFuncErrors(resp.Error, req.Arguments.Get(ctx, &dynamicArg))
+	resp.Error = function.ConcatFuncErrors(resp.Error, req.Arguments.Get(ctx, &resourceType, &configArg, &overridesArgs))
 
-	switch t := dynamicArg.UnderlyingValue().(type) {
-	case types.String:
-		resourceType = t.String()
-		resourceTypeSeen = true
+	switch t := configArg.UnderlyingValue().(type) {
 	case types.Object:
-		obj := dynamicArg.UnderlyingValue().(types.Object)
+		obj := configArg.UnderlyingValue().(types.Object)
 		for key, attr := range obj.Attributes() {
 			switch key {
 			case "name":
 				name = attr.String()
-			case "type":
-				resourceType = attr.String()
-				resourceTypeSeen = true
 			case "location":
 				location = attr.String()
 			case "extra_tokens":
@@ -78,9 +80,24 @@ func (f *CustomNameFunction) Run(ctx context.Context, req function.RunRequest, r
 		resp.Error = function.ConcatFuncErrors(resp.Error, function.NewFuncError(fmt.Sprintf("object expected, got %T", t)))
 	}
 
-	if !resourceTypeSeen {
-		resp.Error = function.ConcatFuncErrors(resp.Error, function.NewFuncError("type is required"))
-		return
+	for _, overrideValue := range overridesArgs {
+        if overrideValue.IsNull() || overrideValue.IsUnknown() {
+            continue
+        }
+        switch t := overrideValue.UnderlyingValue().(type) {
+			case types.Object:
+				obj := overrideValue.UnderlyingValue().(types.Object)
+				for key, attr := range obj.Attributes() {
+					switch key {
+					case "name":
+						name = attr.String()
+					default:
+						resp.Error = function.ConcatFuncErrors(resp.Error, function.NewFuncError(fmt.Sprintf("unknown key %s", key)))
+					}
+				}
+			default:
+				resp.Error = function.ConcatFuncErrors(resp.Error, function.NewFuncError(fmt.Sprintf("object expected, got %T", t)))
+		}
 	}
 
 	resp.Error = function.ConcatFuncErrors(resp.Error, resp.Result.Set(ctx, fmt.Sprintf("name: %s, type: %s, location: %s, extra_tokens: %v", name, resourceType, location, extraTokens)))

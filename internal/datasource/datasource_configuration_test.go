243febafcc728dc454851c0ed57b9324c7d9633f
jason <jason@johnson-net.ch>
2025-01-29 12:48:15 +0100

    test: update variable_maps test case to use new key-value structure

--- internal/datasource/datasource_configuration_test.go
+++ internal/datasource/datasource_configuration_test.go
@@ -94,19 +94,20 @@ func TestAccDataSourceConfiguration_variable_maps(t *testing.T) {
 			{
 				Config: `data "namep_configuration" "example" {
 				  variable_maps = {
-				    "example": {
-				    	"example": "example"
+				    "example_map": {
+				    	"example_key": "example_value"
 					}
 				  }
 				}`,
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownValue(
 						"data.namep_configuration.example",
-						tfjsonpath.New("types"),
-						knownvalue.MapPartial(map[string]knownvalue.Check{
-							"azurerm_resource_group": knownvalue.ObjectPartial(map[string]knownvalue.Check{
-								"name": knownvalue.StringExact("azurerm_resource_group"),
-								"slug": knownvalue.StringExact("rg"),
+						tfjsonpath.New("configuration"),
+						knownvalue.ObjectPartial(map[string]knownvalue.Check{
+							"variable_maps": knownvalue.MapExact(map[string]knownvalue.Check{
+								"example_map": knownvalue.MapExact(map[string]knownvalue.Check{
+									"example_key": knownvalue.StringExact("example_value"),
+								}),
 							}),
 						}),
 					),

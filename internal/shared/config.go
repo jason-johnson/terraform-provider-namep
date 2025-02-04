98b6387630a3470dcd9cf9a09843831a42cebad2
jason <jason@johnson-net.ch>
2025-02-04 21:49:26 +0100

    got rid of a bunch of unused stuff in provider

--- internal/shared/config.go
+++ internal/shared/config.go
@@ -1,14 +1,5 @@
 package shared
 
 type NamepConfig struct {
-	SliceTokens                     []string
-	SliceTokensAvailable            int
-	ExtraVariables                  map[string]string
-	DefaultLocation                 string
-	DefaultResourceNameFormat       string
-	DefaultNodashNameFormat         string
-	DefaultGlobalResourceNameFormat string
-	DefaultGlobalNodashNameFormat   string
-	AzureResourceFormats            map[string]string
-	CustomResourceFormats           map[string]string
+	Static bool
 }
